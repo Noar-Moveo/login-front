f9897966f2799c199bf9e3b029134f58c4605ef2
Noa Reuven <noar@moveo.co.il>
2024-05-22 13:32:03 +0300

    cd

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/HomePage/Home";
 import SignUp from "./pages/SignUp/SignUp";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import SuccessPage from "./pages/SuccessPage/SuccessPage";
-import ResetPassword from "./pages/ResetPassword/PasswordResetSuccess";
+import ResetPassword from "./pages/ResetPassword/ResetPassword";
 // import { createTheme, ThemeProvider } from "@mui/material/styles";
 // import CssBaseline from "@mui/material/CssBaseline";
 import PasswordResetSuccess from "./pages/ResetPassword/PasswordResetSuccess";
